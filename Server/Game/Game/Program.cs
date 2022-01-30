e1ce6480cb16ea167a73ce97ca61853a369f0e35
Koroshiya71 <Koroshiya991125@gmail.com>
2022-01-30 21:31:28 +0800

    提交一版服务器模板

---
+++ Server/Game/Game/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if(!DbManager.Connect("game", "127.0.0.1", 3306, "root", "")){
+				return;
+			}
+
+			NetManager.StartLoop(8888);
+		}
+	}
+}
